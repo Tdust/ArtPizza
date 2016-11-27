02f33560339cdd05ad903452bb8391d4e9e78085
Wanderson Gomes <gomes.tdust@gmail.com>
2016-11-27 04:13:43 -0200

    Adicionando activity monta burguer + correcao de layout

---
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/MontaBurguerActivity.java
@@ -0,0 +1,13 @@
+package iff.edu.br.pooa20162.artpizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MontaBurguerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monta_burguer);
+    }
+}
