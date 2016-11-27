382f3769f978b447241f348fa07a5d3fd028b92c
Wanderson Gomes <gomes.tdust@gmail.com>
2016-11-27 06:17:26 -0200

    Verificando CheckBoxes + codigo de identificao bonzinho

--- app/src/main/java/iff/edu/br/pooa20162/artpizza/MainActivity.java
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/MainActivity.java
@@ -1,7 +1,10 @@
 package iff.edu.br.pooa20162.artpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button montaBurguer = (Button) findViewById(R.id.btMontaBurguer);
+        montaBurguer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, MontaBurguerActivity.class);
+                //intent.putExtra("nome", etNome.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/iff/edu/br/pooa20162/artpizza/MontaBurguerActivity.java
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/MontaBurguerActivity.java
@@ -1,13 +1,83 @@
 package iff.edu.br.pooa20162.artpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MontaBurguerActivity extends AppCompatActivity {
 
+    void adicionar(String adicional)
+    {
+        String msg = adicional+" adicionado";
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+    void remover(String adicional)
+    {
+        String msg = adicional+" removido";
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    void checarAdicional(CheckBox c, String s)
+    {
+        if (c.isChecked())
+        {   adicionar(s);              }
+        else
+        {   remover(s);                }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monta_burguer);
+
+
+
+        final CheckBox bacon = (CheckBox)findViewById(R.id.cbBaconB);
+        final CheckBox queijo = (CheckBox)findViewById(R.id.cbQueijoB);
+        final CheckBox presunto = (CheckBox)findViewById(R.id.cbPresuntoB);
+        final CheckBox ovo = (CheckBox)findViewById(R.id.cbOvoB);
+        final CheckBox frango = (CheckBox)findViewById(R.id.cbFrangoB);
+
+        bacon.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(bacon, "Bacon");
+            }
+        });
+        queijo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(queijo,"Queijo");
+            }
+        });
+        presunto.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(presunto,"Presunto");
+            }
+        });
+        ovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(ovo, "Ovo");
+            }
+        });
+        frango.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(frango,"Frango");
+            }
+        });
     }
 }
