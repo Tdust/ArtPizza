38b8577de41ac19e49fb1ab3d822a4eab5a40e2f
Wanderson Gomes <gomes.tdust@gmail.com>
2016-11-28 22:59:54 -0200

    Implementado o preco + identificao corrigida?

--- app/src/main/java/iff/edu/br/pooa20162/artpizza/Burguer.java
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/Burguer.java
@@ -1,8 +1,118 @@
 package iff.edu.br.pooa20162.artpizza;
 
-/**
- * Created by zelandia on 28/11/2016.
- */
+import android.widget.Toast;
 
 public class Burguer {
+
+    String nome = "hamburguer";
+    boolean frango=false;
+    boolean queijo=false;
+    boolean ovo=false;
+    boolean presunto=false;
+    boolean bacon=false;
+    float preco = 4;
+
+    public String getNome()
+    {
+        if( isBacon() || isOvo() || isFrango() || isPresunto() || isQueijo() )
+        {
+            nome = "burguer";
+        }
+        if (isBacon())
+        {   nome = "Bacon " + nome;
+            this.preco+=1;
+        }
+        if (isPresunto())
+        {
+            nome = "Presunto " + nome;
+            this.preco+=1;
+        }
+        if (isOvo())
+        {
+            nome = "Eggs " + nome;
+            this.preco+=1;
+        }
+        if (isQueijo())
+        {
+            nome = "X " + nome;
+            this.preco+=1;
+        }
+        if (isFrango())
+        {   nome = "Galis " + nome;
+            this.preco+=0.5;
+        }
+        else if( isBacon() && isOvo() && isPresunto() && isQueijo() )
+        {
+            nome = ("X tudo");
+        }
+        else if ( !isBacon() && !isOvo() && !isPresunto() && !isQueijo() )
+        {
+            nome = "Hamburguer";
+        }
+        return this.nome;
+    }
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+    public boolean isQueijo()
+    {
+        return queijo;
+    }
+    public void setQueijo()
+    {
+        this.queijo = !this.queijo;
+    }
+    public boolean isOvo()
+    {
+        return ovo;
+    }
+    public void setOvo()
+    {
+        this.ovo = !this.ovo;
+    }
+    public boolean isPresunto()
+    {
+        return presunto;
+    }
+    public void setPresunto()
+    {
+        this.presunto = !this.presunto;
+    }
+    public boolean isBacon()
+    {
+        return bacon;
+    }
+    public void setBacon()
+    {
+        this.bacon = !this.bacon;
+    }
+    public boolean isFrango()
+    {
+        return frango;
+    }
+    public void setFrango()
+    {
+        this.frango = !this.frango;
+    }
+    public float getPreco()
+    {
+        return preco;
+    }
+    public void setPreco(float preco)
+    {
+        this.preco = preco;
+    }
+
+
+    public Burguer(boolean queijo, boolean ovo, boolean presunto, boolean bacon, boolean frango)
+    {
+        this.bacon = bacon;
+        this.presunto = presunto;
+        this.ovo = ovo;
+        this.queijo = queijo;
+        this.frango = frango;
+    }
+
+    public Burguer(){}
 }
--- app/src/main/java/iff/edu/br/pooa20162/artpizza/MontaBurguerActivity.java
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/MontaBurguerActivity.java
@@ -4,35 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MontaBurguerActivity extends AppCompatActivity {
 
-    void adicionar(String adicional)
+    void adicionar(String adicional, boolean b)
     {
         String msg = adicional+" adicionado";
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        b = true;
     }
-    void remover(String adicional)
+    void remover(String adicional, boolean b)
     {
         String msg = adicional+" removido";
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        b = false;
     }
 
-    void checarAdicional(CheckBox c, String s)
+    void checarAdicional(CheckBox c, String s, boolean b)
     {
         if (c.isChecked())
-        {   adicionar(s);              }
+        {   adicionar(s, b);              }
         else
-        {   remover(s);                }
+        {   remover(s, b);                }
     }
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monta_burguer);
 
-
+        final Burguer lanche = new Burguer();
 
         final CheckBox bacon = (CheckBox)findViewById(R.id.cbBaconB);
         final CheckBox queijo = (CheckBox)findViewById(R.id.cbQueijoB);
@@ -40,43 +47,83 @@ public class MontaBurguerActivity extends AppCompatActivity {
         final CheckBox ovo = (CheckBox)findViewById(R.id.cbOvoB);
         final CheckBox frango = (CheckBox)findViewById(R.id.cbFrangoB);
 
-        bacon.setOnClickListener(new View.OnClickListener()
-        {
+        final boolean ba=false, qu= false, pr=false, eg=false, fr=false;
+
+        bacon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                checarAdicional(bacon, "Bacon");
+                checarAdicional(bacon, "Bacon", ba);
+                lanche.setBacon();
             }
         });
-        queijo.setOnClickListener(new View.OnClickListener()
-        {
+        queijo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                checarAdicional(queijo,"Queijo");
+                checarAdicional(queijo,"Queijo", qu);
+                lanche.setQueijo();
             }
         });
-        presunto.setOnClickListener(new View.OnClickListener()
-        {
+        presunto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                checarAdicional(presunto,"Presunto");
+                checarAdicional(presunto,"Presunto", pr);
+                lanche.setPresunto();
             }
         });
         ovo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                checarAdicional(ovo, "Ovo");
+                checarAdicional(ovo, "Ovo", eg);
+                lanche.setOvo();
             }
         });
-        frango.setOnClickListener(new View.OnClickListener()
+        frango.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                checarAdicional(frango,"Frango", fr);
+                lanche.setFrango();
+            }
+        });
+
+        final Button bok = (Button)findViewById(R.id.btOK);
+        final Button blimpar = (Button)findViewById(R.id.btLimpar);
+        final TextView tv = (TextView)findViewById(R.id.tvResultado);
+        final TextView txvPreco = (TextView)findViewById(R.id.txvPreco);
+
+        bok.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                checarAdicional(frango,"Frango");
+                lanche.setPreco(4);
+                tv.setText(lanche.getNome(), null);
+                txvPreco.setText("R$ "+Float.toString(lanche.getPreco())+"0");
+            }
+        });
+
+        blimpar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                tv.setText("Clique em OK para ver o nome do lanche", null);
+                lanche.setPreco(4);
+                txvPreco.setText("R$ "+Float.toString(lanche.getPreco())+"0");
+                if(bacon.isChecked())
+                {   bacon.toggle();     }
+                if(queijo.isChecked())
+                {   queijo.toggle();    }
+                if(presunto.isChecked())
+                {   presunto.toggle();  }
+                if(ovo.isChecked())
+                {   ovo.toggle();       }
+                if(frango.isChecked())
+                {   frango.toggle();    }
             }
         });
     }
