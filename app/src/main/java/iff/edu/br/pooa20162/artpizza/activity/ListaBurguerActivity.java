6618566206d731623becc536f67bf0108ec4b45b
Wanderson Gomes <gomes.tdust@gmail.com>
2017-02-21 11:19:17 -0300

    Novas telas e classes sem conexao

---
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/activity/ListaBurguerActivity.java
@@ -0,0 +1,15 @@
+package iff.edu.br.pooa20162.artpizza.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import iff.edu.br.pooa20162.artpizza.R;
+
+public class ListaBurguerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_burguer);
+    }
+}
---
+++ app/src/main/java/iff/edu/br/pooa20162/artpizza/model/Pizza.java
@@ -0,0 +1,8 @@
+package iff.edu.br.pooa20162.artpizza.model;
+
+/**
+ * Created by zelandia on 21/02/2017.
+ */
+
+public class Pizza {
+}
